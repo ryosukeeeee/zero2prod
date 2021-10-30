d2898830e05457a719296bf5d0dc04c77ba9abb4
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-30 16:11:36 +0900

    refactor: change position of function

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,6 +9,18 @@ use std::net::TcpListener;
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future.
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
@@ -30,16 +42,4 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future.
-    // but we have no use for it here, hence the non-binding let
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
 }
