ab8dc8f54f4a6844bd7dfdff8e7855d244b33b97
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-30 15:36:54 +0900

    feat: add test

---
+++ tests/health_check.rs
@@ -0,0 +1,39 @@
+// `actix_rt::test` is the testing equivalent of `actix_web::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// Use `cargo add actix-rt --dev --vers 2` to add `actix-rt`
+// under `[dev-dependencies]` in Cargo.toml
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    //
+    // use `cargo add reqwest --dev --vers 0.11` to add
+    // it under `[dev-dependencies]` in Cargo.toml
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future.
+    // but we have no use for it here, hence the non-binding let
+    let _ = tokio::spawn(server);
+}
