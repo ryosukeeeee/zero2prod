d26f8d8cfa74ca5ae774cfbd4f7c5e6ebdc75998
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-30 15:58:58 +0900

    feat: when test, change to use random port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 // `actix_rt::test` is the testing equivalent of `actix_web::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -10,7 +12,7 @@
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     //
@@ -20,7 +22,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -30,10 +32,14 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future.
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
