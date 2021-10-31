023a7100470a125845ed4bf58317c687a4f799af
ryosukeeeee <ryou.fm.bros@gmail.com>
2021-10-31 17:52:30 +0900

    feat: make space

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,5 @@
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    // Notice the different signature!
-    // We return `Server` on the happy path and we dropped the `async` keyword
-    // We have no .await call, so it is not needed anymore.
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    // No .await here!
-    Ok(server)
-}
+pub use crate::startup::run;
---
+++ src/startup.rs
@@ -0,0 +1,19 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    // Notice the different signature!
+    // We return `Server` on the happy path and we dropped the `async` keyword
+    // We have no .await call, so it is not needed anymore.
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    // No .await here!
+    Ok(server)
+}
